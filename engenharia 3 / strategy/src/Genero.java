3fcc1d362b453350065818c527c3f8c426320850
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-16 15:44:47 -0300

    Create Genero.java

---
+++ engenharia 3 / strategy/src/Genero.java
@@ -0,0 +1,6 @@
+public class RecomendacaoPorGenero implements RecomendacaoStrategy {
+    @Override
+    public void recomendarFilme() {
+        System.out.println("Recomendando filmes de Romance...");
+    }
+}
