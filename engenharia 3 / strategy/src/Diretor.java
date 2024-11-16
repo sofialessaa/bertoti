585f7b7217cc2b4f63708a75bad7660c024166b5
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-16 15:46:27 -0300

    Create Diretor.java

---
+++ engenharia 3 / strategy/src/Diretor.java
@@ -0,0 +1,6 @@
+public class RecomendacaoPorDiretor implements RecomendacaoStrategy {
+    @Override
+    public void recomendarFilme() {
+        System.out.println("Recomendando filmes do diretor Jordan Peele...");
+    }
+}
