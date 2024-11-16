a603b83bd124599e79ef72b8890aed36bd28424a
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-16 15:47:43 -0300

    Create Avaliacao.java

---
+++ engenharia 3 / strategy/src/Avaliacao.java
@@ -0,0 +1,6 @@
+public class Avaliacao implements Recomendacao {
+    @Override
+    public void recomendarFilme() {
+        System.out.println("Recomendando filmes com avaliação acima de 3 estrelas...");
+    }
+}
