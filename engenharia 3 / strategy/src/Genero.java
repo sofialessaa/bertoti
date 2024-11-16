6dea3075d3db83137c03596380da8e13a1e2c150
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-16 15:47:14 -0300

    Update Genero.java

--- engenharia 3 / strategy/src/Genero.java
+++ engenharia 3 / strategy/src/Genero.java
@@ -1,4 +1,4 @@
-public class RecomendacaoPorGenero implements RecomendacaoStrategy {
+public class Genero implements Recomendacao {
     @Override
     public void recomendarFilme() {
         System.out.println("Recomendando filmes de Romance...");
