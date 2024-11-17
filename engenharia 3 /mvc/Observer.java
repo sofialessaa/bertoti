7dd82c65ced2a10917cfe7d18ffd51c4f798cec3
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 18:09:57 -0300

    Update Observer.java

--- engenharia 3 /mvc/Observer.java
+++ engenharia 3 /mvc/Observer.java
@@ -1,3 +1,16 @@
 public interface Observer {
     void atualizar(String mensagem);
 }
+
+class Cliente implements Observer {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void atualizar(String mensagem) {
+        System.out.println("Notificação para " + nome + ": " + mensagem + ".");
+    }
+}
