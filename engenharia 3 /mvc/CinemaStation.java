b1acda4bd2d46e8800cca42df9efa735f0286023
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 18:12:10 -0300

    Update CinemaStation.java

--- engenharia 3 /mvc/CinemaStation.java
+++ engenharia 3 /mvc/CinemaStation.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class CinemaStation {
     public static void main(String[] args) {
-        // Configuração inicial
         Filme filme1 = new Filme("Avatar", "Aventura", 162);
         Filme filme2 = new Filme("O Senhor dos Anéis", "Fantasia", 201);
 
         Sessao sessao1 = new Sessao(filme1, "19:30", 30);
         Sessao sessao2 = new Sessao(filme2, "20:50", 25);
 
-        List<Sessao> sessoes = new ArrayList<>();
-        sessoes.add(sessao1);
-        sessoes.add(sessao2);
+        Cliente cliente1 = new Cliente("Fernando");
+        Cliente cliente2 = new Cliente("Sofia");
 
-        Visao visao = new Visao();
-        ControllerTrial controller = new ControllerTrial(sessoes, visao);
+        sessao1.adicionarCliente(cliente1);
+        sessao1.adicionarCliente(cliente2);
 
-        // Iniciar o sistema
-        controller.iniciar();
-
-        // Simulação de reserva
-        controller.reservarLugar(0, "Fernando");
-        controller.reservarLugar(1, "Sofia");
-        controller.reservarLugar(1, "Ana Luiza");
+        // Testar notificações
+        sessao1.reservarLugar();  
+        sessao1.removerCliente(cliente1);
+        sessao1.reservarLugar();
     }
 }
