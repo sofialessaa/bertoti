8919e1b13a8aa32adb8883905376b4399fcc7fbc
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 12:55:12 -0300

    Create Leaf.java

---
+++ engenharia 3 /composite/src/Leaf.java
@@ -0,0 +1,12 @@
+class Leaf implements Component {
+    private String name;
+
+    public Leaf(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void operation() {
+        System.out.println("Leaf: " + name);
+    }
+}
