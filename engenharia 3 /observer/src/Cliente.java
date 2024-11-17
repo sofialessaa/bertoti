d6dd6ed685427c8753cc687f7bf56560599d52fa
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 11:45:05 -0300

    Create Cliente.java

---
+++ engenharia 3 /observer/src/Cliente.java
@@ -0,0 +1,12 @@
+class Cliente implements Cliente {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void receberNotificacao(String mensagem) {
+        System.out.println("Cliente " + nome + " recebeu: " + mensagem + ".");
+    }
+}
