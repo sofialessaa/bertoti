a341f532bd66708a87c6d95984a293a3da8f91f6
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 13:14:48 -0300

    Create Main.java

---
+++ engenharia 3 /singleton/src/Main.java
@@ -0,0 +1,10 @@
+    public static void main(String[] args) {
+        CinemaSessionManager manager = CinemaSessionManager.getInstance();
+
+        manager.addSession("Ainda Estou Aqui - 19:00");
+        manager.addSession("Todo Tempo Que Temos - 21:30");
+        manager.addSession("A Forja - 16:00");
+
+        manager.listSessions();
+    }
+}
