b341be75cad3d35068baa699652ec603d8a0e33d
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-11-17 13:59:51 -0300

    Create GerenciadorReservas.java

---
+++ engenharia 3 /facade/src/GerenciadorReservas.java
@@ -0,0 +1,5 @@
+public class GerenciadorReservas {
+    public void reservarAssento(String filme, String assento) {
+        System.out.println("Reserva realizada: " + filme + ", Assento: " + assento);
+    }
+}
